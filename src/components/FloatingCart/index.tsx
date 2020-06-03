eb06e52d8e6abe564e273827a8cfbf336e88cbf6
Ricardo Nascimento <ricardobatistanf@gmail.com>
2020-06-02 23:22:12 -0300

    Finish the challenge

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((totalPrice, product) => {
+      const productsPrice = product.quantity * product.price;
 
-    return formatValue(0);
+      return totalPrice + productsPrice;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((totalItens, product) => {
+      const productQtd = product.quantity;
 
-    return 0;
+      return totalItens + productQtd;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
